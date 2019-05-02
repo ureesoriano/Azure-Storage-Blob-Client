d04a78487ee0ede891dbb3feb56139104d91de8b
Oriol Soriano <oriol.soriano@capside.com>
2019-05-02 16:00:28 +0200

    Migrate the responsability of parsing response to the Call class
    The Azure Storage Blob API returns responses in various formats and in
    different parts of the HTTP response (sometimes in the content,
    sometimes in the headers).
    At this stage, it seems reasonable to store the state of in which format
    a given API call returns the result and in which part of the HTTP
    response in the Call class, as it gathers all the specific knowledge
    around an API call.

--- lib/Azure/Storage/Blob/Client/ListBlobs.pm
+++ lib/Azure/Storage/Blob/Client/ListBlobs.pm
@@ -1,6 +1,7 @@
 package Azure::Storage::Blob::Client::ListBlobs;
 use Moose;
 use Azure::Storage::Blob::Client::Meta::Attribute::Custom::Trait::CallParameter;
+use XML::LibXML;
 
 has 'endpoint' => (is => 'ro', init_arg => undef, lazy => 1, default => sub {
   my $self = shift;
@@ -18,6 +19,15 @@ has container => (is => 'ro', isa => 'Str', required => 1);
 has account_name => (is => 'ro', isa => 'Str', required => 1);
 has account_key => (is => 'ro', isa => 'Str', required => 1);
 has prefix => (is => 'ro', isa => 'Str', traits => ['CallParameter'], required => 1);
+
+sub parse_response {
+  my ($self, $response) = @_;
+  my $dom = XML::LibXML->load_xml(string => $response->content);
+
+  return [
+    map { $_->to_literal() } $dom->findnodes('/EnumerationResults/Blobs/Blob/Name')
+  ];
+}
 
 __PACKAGE__->meta->make_immutable();
 
